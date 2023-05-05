e4b1a92400c497439355593a9c1faa94c4bea238
timge <xiang.ge@agvsz.com>
2023-05-05 18:16:53 +0800

    删除 vs

--- AspNetCore/Security/Authentication/IdentitySample.DefaultUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentitySample.DefaultUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
