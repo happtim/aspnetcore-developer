d6198881da13eb1d41b9b12c7fe36b039482f910
timge <gexiang@gmail.com>
2023-07-17 14:46:17 +0800

    openiddict clientCredentials

---
+++ Frameworks/Openiddict/1_ClientCredentials/src/Api2/Program.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+//using OpenIddict.Validation.AspNetCore;
+
+using OpenIddict.Validation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ builder.Services.AddOpenIddict()
+     .AddValidation(options =>
+     {
+         // Note: the validation handler uses OpenID Connect discovery
+         // to retrieve the address of the introspection endpoint.
+         options.SetIssuer("https://localhost:5001/");
+         options.AddAudiences("resource_server_1");
+
+         // Register the System.Net.Http integration.
+         options.UseSystemNetHttp();
+
+         // Register the ASP.NET Core host.
+         options.UseAspNetCore();
+     });
+
+builder.Services.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+
+builder.Services.AddAuthorization(options =>
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "api1");
+    })
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers().RequireAuthorization("ApiScope");
+
+app.Run();
---
+++ Frameworks/Openiddict/1_ClientCredentials/src/openiddictServer/Startup.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenIddict.Abstractions;
+using openiddictServer.Models;
+using Quartz;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace openiddictServer;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+        => Configuration = configuration;
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            // Configure the context to use sqlite.
+            options.UseSqlite($"Filename=openiddict-aridka-server.db");
+
+            // Register the entity sets needed by OpenIddict.
+            // Note: use the generic overload if you need
+            // to replace the default OpenIddict entities.
+            options.UseOpenIddict();
+        });
+
+        services.AddOpenIddict()
+
+            // Register the OpenIddict core components.
+            .AddCore(options =>
+            {
+                // Configure OpenIddict to use the Entity Framework Core stores and models.
+                // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<ApplicationDbContext>();
+                
+            })
+
+            // Register the OpenIddict server components.
+            .AddServer(options =>
+            {
+                // Enable the token endpoint.
+                options
+                    .SetTokenEndpointUris("connect/token");
+                    //.SetIntrospectionEndpointUris("connect/introspect");
+
+                // Enable the client credentials flow.
+                options.AllowClientCredentialsFlow();
+
+                // Register the signing and encryption credentials.
+                options
+                    .AddEphemeralEncryptionKey()
+                    .AddEphemeralSigningKey()
+                    .DisableAccessTokenEncryption();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options.UseAspNetCore()
+                    .EnableTokenEndpointPassthrough();
+            });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+        
+        InitializeDatabaseAsync(app);
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapDefaultControllerRoute();
+        });
+
+        app.UseWelcomePage();
+    }
+    
+    private static async void InitializeDatabaseAsync(IApplicationBuilder app)
+    {
+        await using var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateAsyncScope();
+    
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        
+        await RegisterApplicationsAsync(scope.ServiceProvider);
+        await RegisterScopesAsync(scope.ServiceProvider);
+
+        static async Task RegisterApplicationsAsync(IServiceProvider provider)
+        {
+            var manager = provider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            // API
+            if (await manager.FindByClientIdAsync("client") == null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    DisplayName = "My client application",
+                    Permissions =
+                    {
+                        Permissions.Endpoints.Token,
+                        Permissions.GrantTypes.ClientCredentials,
+                        Permissions.Prefixes.Scope + "api1"
+                    }
+                });
+            }
+        }
+        
+        static async Task RegisterScopesAsync(IServiceProvider provider)
+        {
+            var manager = provider.GetRequiredService<IOpenIddictScopeManager>();
+
+            if (await manager.FindByNameAsync("api1") is null)
+            {
+                await manager.CreateAsync(new OpenIddictScopeDescriptor
+                {
+                    DisplayName = "Dantooine API access",
+                    DisplayNames =
+                    {
+                        [CultureInfo.GetCultureInfo("fr-FR")] = "Accès à l'API de démo"
+                    },
+                    Name = "api1",
+                    Resources =
+                    {
+                        //Assign the aud value to the resource parameter.
+                        "resource_server_1"
+                    }
+                });
+            }
+        }
+
+
+        
+    }
+}
