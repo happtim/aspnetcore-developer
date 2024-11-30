b2122a994402e82003fa83180d04bf9886e14a77
timge <gexiang@gmail.com>
2024-11-30 14:04:49 +0800

    blazor livechart2

---
+++ UI/Blazor/.Net7/BlazorLiveCharts2/Pages/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+
+namespace BlazorLiveCharts2.Pages
+{
+    public class ViewModel
+    {
+        public ISeries[] Series { get; set; } = new ISeries[] {
+          new LineSeries<double>
+                {
+                    Values = new List<double>() { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null,
+                    GeometrySize = 20
+                }
+        };
+
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
+            {
+                Text = "My chart title",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15)
+            };
+    }
+}
