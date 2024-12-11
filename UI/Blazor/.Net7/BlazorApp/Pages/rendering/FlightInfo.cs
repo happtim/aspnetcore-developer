3974e9737404a27e04a77c7f4fb5439da06f15d0
timge <gexiang@gmail.com>
2024-12-11 22:38:50 +0800

    5_OptimizeRendering

---
+++ UI/Blazor/.Net7/BlazorApp/Pages/rendering/FlightInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Pages.rendering
+{
+    public class FlightInfo
+    {
+        public int FlightId { get; set; }
+        public string Airline { get; set; }
+        public string Destination { get; set; }
+        // 其他相关属性  
+    }
+}
